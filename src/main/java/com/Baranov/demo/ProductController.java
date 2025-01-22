1bcde78576c0b8aebe772d066ff07725df66783f
Oleksandr-star <76440362+Oleksandr-star@users.noreply.github.com>
2025-01-22 12:57:49 +0200

    Lr_1

---
+++ src/main/java/com/Baranov/demo/ProductController.java
@@ -0,0 +1,31 @@
+package com.Baranov.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.Baranov.demo.model.Product;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final List<Product> products = new ArrayList<>();
+
+    public ProductController() {
+        products.add(new Product(1L, "Laptop", "Electronics", "A powerful laptop", 1200.0, 5));
+        products.add(new Product(2L, "Phone", "Electronics", "A modern smartphone", 800.0, 10));
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return products;
+    }
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+}
